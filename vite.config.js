a9a6b792c981d6cb46bde6198f8450f0d1c59ec1
hpdinh <haydenpdinh@gmail.com>
2025-07-24 18:01:02 -0700

    Project finished

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/hayden-dinh-portfolio-tutorial",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
