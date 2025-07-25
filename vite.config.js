541b5149b603a5675d54c23a913c796d9435c493
hpdinh <haydenpdinh@gmail.com>
2025-07-24 17:29:58 -0700

    adding ability to host on github pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/hayden-dinh-portfolio-tutorial",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
